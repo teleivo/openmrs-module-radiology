8d1b09450cae4a094faa6c80780fc0ab0551c86c
teleivo <mrteleivo@gmail.com>
2016-04-22 16:11:45 +0200

    undone

---
+++ api/src/main/java/org/openmrs/module/radiology/dicom/PersistentMppsSCP.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.radiology.dicom;
+
+import java.io.IOException;
+
+public class PersistentMppsSCP extends MppsSCP {
+	
+	private static final PersistentMppsSCP persistentMppsSCP = new PersistentMppsSCP();
+
+	public PersistentMppsSCP() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public 
+	
+}
