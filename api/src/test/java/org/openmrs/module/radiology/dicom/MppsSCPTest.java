6a9071aeab249c79528e120a67d82b3b84f85dc5
teleivo <mrteleivo@gmail.com>
2016-04-25 16:51:50 +0200

    UNDONE add constructor for our use and use it in RadiologyActivator

---
+++ api/src/test/java/org/openmrs/module/radiology/dicom/MppsSCPTest.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.radiology.dicom;
+
+import org.junit.Test;
+
+public class MppsSCPTest {
+	
+	@Test
+	public void testme() {
+		// configureBindServer connection, ae
+		// configure ae.addTransferCapability
+		// configure IODs (setMppsNCreateIOD, setMppsNSetIOD)
+		// set storageDirectory
+	}
+}
