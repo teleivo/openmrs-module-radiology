76bd266685cfd5dc3c0a83f674e012315db404cd
teleivo <mrteleivo@gmail.com>
2016-05-28 18:36:54 +0200

    fix

--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -1,5 +1,7 @@
 describe('The radiology order list', function() {
 
+  jasmine.getFixtures().fixturesPath = 'spec/fixtures';
+  
   loadFixtures('orderSearch.portlet.html');
 
   it('should have datatables defined', function() {
