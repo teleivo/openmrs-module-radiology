f7db0f685503b3c34f18cd60c2bf62cec39fe833
teleivo <mrteleivo@gmail.com>
2016-05-28 18:54:53 +0200

    undefined is not a constructor

--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -1,10 +1,10 @@
 describe('The radiology order list', function() {
 
   jasmine.getFixtures().fixturesPath = 'spec/fixtures';
-  
-  loadFixtures('orderSearch.portlet.html');
+
+  // loadFixtures('orderSearch.portlet.html');
 
   it('should have datatables defined', function() {
-
+    expect($('<div><span></span></div>')).toHaveHtml('<span></span>');
   });
 });
