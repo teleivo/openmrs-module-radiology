e80cbb9e244fa480358008f472c13606ae130a31
teleivo <mrteleivo@gmail.com>
2016-05-29 14:03:42 +0200

    first run :)

--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -1,13 +1,17 @@
 describe("The radiology order list", function() {
 
-  //  jasmine.getFixtures().fixturesPath = "spec/fixtures";
+  jasmine.getFixtures().fixturesPath = "spec/fixtures";
 
-  //  loadFixtures('orderSearch.portlet.html');
+  beforeEach(function() {
+    jasmine.Clock.useMock();
+    jasmine.Ajax.useMock();
+    loadFixtures('radiologyOrder.list.html');
+  });
 
   it("should have datatables defined", function() {
     expect(true).toBe(true);
 
     expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
-//    expect(jQuery.("<div><span></span></div>")).toHaveHtml("<span></span>");
+    // expect(jQuery.("<div><span></span></div>")).toHaveHtml("<span></span>");
   });
 });
