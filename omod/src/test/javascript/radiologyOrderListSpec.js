ebe43daac6243fae517e8f2f17445f1936adaad5
teleivo <mrteleivo@gmail.com>
2016-05-29 22:10:34 +0200

    hoho

--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -11,18 +11,23 @@ describe("The radiology order list", function() {
     request = mostRecentAjaxRequest();
   });
 
-  it("should show the loading element", function(){
+  it("should show the loading element", function() {
     expect(jQuery('div#openmrs_msg[name="loading"]')).toBeVisible();
   });
 
-  it("should call the orderSearch portlet url", function(){
+  it("should call the orderSearch portlet url", function() {
     expect(request.url).toEqual("portlets/orderSearch.portlet")
   });
 
   it("should have define datatables", function() {
-    expect(true).toBe(true);
-
+//    expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
     expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
-    // expect(jQuery.("<div><span></span></div>")).toHaveHtml("<span></span>");
+  });
+
+  it("should clear search box on clearResults click", function() {
+    jQuery("table#searchForm input:text").val("COMPLETED");
+    jQuery("tbody#radiologyOrdersTableBody").html("table data");
+    jQuery("a#clearResults").click();
+    expect(jQuery("table#searchForm input:text")).toBeEmpty();
   });
 });
