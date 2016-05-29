3fa2912fb3e699e05850270a62865492ff26b4a2
teleivo <mrteleivo@gmail.com>
2016-05-29 11:55:07 +0200

    fix preloadsources

--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -1,11 +1,13 @@
 describe("The radiology order list", function() {
 
-//  jasmine.getFixtures().fixturesPath = "spec/fixtures";
+  //  jasmine.getFixtures().fixturesPath = "spec/fixtures";
 
-//  loadFixtures('orderSearch.portlet.html');
+  //  loadFixtures('orderSearch.portlet.html');
 
   it("should have datatables defined", function() {
     expect(true).toBe(true);
-    expect($("<div><span></span></div>")).toHaveHtml("<span></span>");
+
+//    expect($("<div><span></span></div>")).toHaveHtml("<span></span>");
+    expect(jQuery.("<div><span></span></div>")).toHaveHtml("<span></span>");
   });
 });
