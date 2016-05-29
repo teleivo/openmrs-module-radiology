cf9374409bc4f21d6bd39d95e53f5c4bf3cc1ceb
teleivo <mrteleivo@gmail.com>
2016-05-29 14:15:41 +0200

    add simple test

--- omod/src/main/webapp/resources/js/radiologyOrderList.js
+++ omod/src/main/webapp/resources/js/radiologyOrderList.js
@@ -43,101 +43,101 @@ $j(document)
 
                     loading.show();
                     $j('#errorSpan').html('');
-                    //                    $j
-                    //                            .get(
-                    //                                    'portlets/orderSearch.portlet',
-                    //                                    {
-                    //                                      patientQuery: pQuery.val(),
-                    //                                      startDate: startDate.val(),
-                    //                                      endDate: endDate.val()
-                    //                                    },
-                    //                                    function(data) {
-                    //                                      loading.hide();
-                    //                                      // crossDate error span rendered/sended
-                    //                                      // from portlet
-                    //                                      if (data.match(/crossDate/ig) != null) {
-                    //                                        $j('#errorSpan').html($j(data));
-                    //                                      }
-                    //                                      // no errors
-                    //                                      else {
-                    //                                        // load data
-                    //                                        results.html(data);
-                    //                                        var oTable = $j(
-                    //                                                'table#radiologyOrdersTable')
-                    //                                                .DataTable(
-                    //                                                        {
-                    //                                                          "order": [[1, 'asc']],
-                    //                                                          "oLanguage": {
-                    //                                                            "sLengthMenu": '<spring:message code="radiology.show"/>'
-                    //                                                                    + ' _MENU_ <spring:message code="radiology.entries"/>',
-                    //                                                            "sSearch": '<spring:message code="general.search"/>:',
-                    //                                                            "sInfo": '<spring:message code="radiology.viewing"/> _START_ '
-                    //                                                                    + '- _END_ '
-                    //                                                                    + '<spring:message code="radiology.of"/> _TOTAL_',
-                    //                                                            "oPaginate": {
-                    //                                                              "sFirst": '<spring:message code="radiology.first"/>',
-                    //                                                              "sPrevious": '<spring:message code="general.previous"/>',
-                    //                                                              "sNext": '<spring:message code="general.next"/>',
-                    //                                                              "sLast": '<spring:message code="radiology.last"/>',
-                    //                                                            },
-                    //                                                            "sProcessing": '<spring:message code="general.loading"/>'
-                    //                                                          },
-                    //                                                          "columnDefs": [
-                    //                                                              {
-                    //                                                                "sType": "num-html",
-                    //                                                                "bSortable": true,
-                    //                                                                "aTargets": [0]
-                    //                                                              },
-                    //                                                              {
-                    //                                                                "targets": [8],
-                    //                                                                "visible": false,
-                    //                                                                "searchable": false
-                    //                                                              },
-                    //                                                              {
-                    //                                                                "targets": [9],
-                    //                                                                "visible": false,
-                    //                                                                "searchable": false
-                    //                                                              },
-                    //                                                              {
-                    //                                                                "targets": [10],
-                    //                                                                "visible": false,
-                    //                                                                "searchable": false
-                    //                                                              }]
-                    //                                                        });
-                    //                                        $j('#radiologyOrdersTableBody')
-                    //                                                .on(
-                    //                                                        'click',
-                    //                                                        'tr',
-                    //                                                        function() {
-                    //                                                          var tr = $j(this);
-                    //                                                          var row = oTable
-                    //                                                                  .row(this);
-                    //
-                    //                                                          if (row.child
-                    //                                                                  .isShown()) {
-                    //                                                            row.child.hide();
-                    //                                                            tr
-                    //                                                                    .removeClass('shown');
-                    //                                                          } else {
-                    //                                                            row
-                    //                                                                    .child(
-                    //                                                                            format(
-                    //                                                                                    tr
-                    //                                                                                            .data('child-order_id'),
-                    //                                                                                    tr
-                    //                                                                                            .data('child-physician'),
-                    //                                                                                    tr
-                    //                                                                                            .data('child-status'),
-                    //                                                                                    tr
-                    //                                                                                            .data('child-instructions')),
-                    //                                                                            'no-padding')
-                    //                                                                    .show();
-                    //                                                            tr
-                    //                                                                    .addClass('shown');
-                    //                                                          }
-                    //                                                        });
-                    //                                      }
-                    //                                    });
+                                        $j
+                            .get(
+                                    'portlets/orderSearch.portlet',
+                                    {
+                                      patientQuery: pQuery.val(),
+                                      startDate: startDate.val(),
+                                      endDate: endDate.val()
+                                    },
+                                    function(data) {
+                                      loading.hide();
+                                      // crossDate error span rendered/sended
+                                      // from portlet
+                                      if (data.match(/crossDate/ig) != null) {
+                                        $j('#errorSpan').html($j(data));
+                                      }
+                                      // no errors
+                                      else {
+                                        // load data
+                                        results.html(data);
+                                        var oTable = $j(
+                                                'table#radiologyOrdersTable')
+                                                .DataTable(
+                                                        {
+                                                          "order": [[1, 'asc']],
+                                                          "oLanguage": {
+                                                            "sLengthMenu": '<spring:message code="radiology.show"/>'
+                                                                    + ' _MENU_ <spring:message code="radiology.entries"/>',
+                                                            "sSearch": '<spring:message code="general.search"/>:',
+                                                            "sInfo": '<spring:message code="radiology.viewing"/> _START_ '
+                                                                    + '- _END_ '
+                                                                    + '<spring:message code="radiology.of"/> _TOTAL_',
+                                                            "oPaginate": {
+                                                              "sFirst": '<spring:message code="radiology.first"/>',
+                                                              "sPrevious": '<spring:message code="general.previous"/>',
+                                                              "sNext": '<spring:message code="general.next"/>',
+                                                              "sLast": '<spring:message code="radiology.last"/>',
+                                                            },
+                                                            "sProcessing": '<spring:message code="general.loading"/>'
+                                                          },
+                                                          "columnDefs": [
+                                                              {
+                                                                "sType": "num-html",
+                                                                "bSortable": true,
+                                                                "aTargets": [0]
+                                                              },
+                                                              {
+                                                                "targets": [8],
+                                                                "visible": false,
+                                                                "searchable": false
+                                                              },
+                                                              {
+                                                                "targets": [9],
+                                                                "visible": false,
+                                                                "searchable": false
+                                                              },
+                                                              {
+                                                                "targets": [10],
+                                                                "visible": false,
+                                                                "searchable": false
+                                                              }]
+                                                        });
+                                        $j('#radiologyOrdersTableBody')
+                                                .on(
+                                                        'click',
+                                                        'tr',
+                                                        function() {
+                                                          var tr = $j(this);
+                                                          var row = oTable
+                                                                  .row(this);
+
+                                                          if (row.child
+                                                                  .isShown()) {
+                                                            row.child.hide();
+                                                            tr
+                                                                    .removeClass('shown');
+                                                          } else {
+                                                            row
+                                                                    .child(
+                                                                            format(
+                                                                                    tr
+                                                                                            .data('child-order_id'),
+                                                                                    tr
+                                                                                            .data('child-physician'),
+                                                                                    tr
+                                                                                            .data('child-status'),
+                                                                                    tr
+                                                                                            .data('child-instructions')),
+                                                                            'no-padding')
+                                                                    .show();
+                                                            tr
+                                                                    .addClass('shown');
+                                                          }
+                                                        });
+                                      }
+                                    });
                   }
 
                   // ***********Events*************
--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -1,14 +1,25 @@
 describe("The radiology order list", function() {
+  var request;
 
   jasmine.getFixtures().fixturesPath = "spec/fixtures";
 
   beforeEach(function() {
-    jasmine.Clock.useMock();
-    jasmine.Ajax.useMock();
     loadFixtures('radiologyOrder.list.html');
+
+    jasmine.Ajax.useMock();
+
+    request = mostRecentAjaxRequest();
   });
 
-  it("should have datatables defined", function() {
+  it("should show the loading element", function(){
+    expect(jQuery('div#openmrs_msg[name="loading"]')).toBeVisible();
+  });
+
+  it("should call the orderSearch portlet url", function(){
+    expect(request.url).toEqual("portlets/orderSearch.portlet")
+  });
+
+  it("should have define datatables", function() {
     expect(true).toBe(true);
 
     expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
