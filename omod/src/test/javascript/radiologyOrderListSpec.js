31dfa06ec8b7e08380f9583abc5cc84ab32d4e77
teleivo <mrteleivo@gmail.com>
2016-06-06 14:07:56 +0200

    make testable

--- omod/src/test/javascript/radiologyOrderListSpec.js
+++ omod/src/test/javascript/radiologyOrderListSpec.js
@@ -1,57 +1,64 @@
-describe("The radiology order list", function() {
-  var request;
-  var patientQuery;
-  var startDate;
-  var endDate;
-  var clearResults;
+describe(
+        "The radiology order list",
+        function() {
+          var request;
+          var patientQuery;
+          var startDate;
+          var endDate;
+          var clearResults;
 
-  jasmine.getFixtures().fixturesPath = "spec/fixtures";
+          jasmine.getFixtures().fixturesPath = "spec/fixtures";
 
-  beforeEach(function() {
-    loadFixtures('radiologyOrder.list.html');
+          beforeEach(function() {
+            loadFixtures('radiologyOrder.list.html');
 
-    //clearResults = jQuery("a#clearResults");
-    clearResults = $j("a#clearResults");
+            //clearResults = jQuery("a#clearResults");
+            clearResults = $j("a#clearResults");
 
-    patientQuery = $j('input[name="patientQuery"]');
-    startDate = jQuery('input[name="startDate"]');
-    endDate = jQuery('input[name="endDate"]');
-    patientQuery.val("Hornblower");
-    startDate.val("2016-05-01");
-    endDate.val("2016-05-31");
+            patientQuery = $j('input[name="patientQuery"]');
+            startDate = jQuery('input[name="startDate"]');
+            endDate = jQuery('input[name="endDate"]');
+            patientQuery.val("Hornblower");
+            startDate.val("2016-05-01");
+            endDate.val("2016-05-31");
+            patientQuery.val("XXX");
 
-    //jQuery("tbody#radiologyOrdersTableBody").html("table data");
+            //jQuery("tbody#radiologyOrdersTableBody").html("table data");
 
-    jasmine.Ajax.useMock();
+            jasmine.Ajax.useMock();
 
-    request = mostRecentAjaxRequest();
-  });
+            request = mostRecentAjaxRequest();
+          });
 
-  it("should show the loading element", function() {
-    expect(jQuery('div#openmrs_msg[name="loading"]')).toBeVisible();
-  });
+          it("should show the loading element", function() {
+            expect(jQuery('div#openmrs_msg[name="loading"]')).toBeVisible();
+          });
 
-  //it("should call the orderSearch portlet url", function() {
-    //expect(request.url).toEqual("portlets/orderSearch.portlet")
-  //});
+          //it("should call the orderSearch portlet url", function() {
+          //expect(request.url).toEqual("portlets/orderSearch.portlet")
+          //});
 
-  it("should have define datatables", function() {
-    //    expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
-    expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
-  });
+          it("should have define datatables", function() {
+            //    expect(jQuery("<div><span></span></div>")).toHaveHtml("<span></span>");
+            expect(jQuery("<div><span></span></div>")).toHaveHtml(
+                    "<span></span>");
+          });
 
-  describe("clicking the clearResults anchor with values in search box text inputs", function() {
-    it("should clear search box on clearResults click", function() {
-      spyEvent = spyOnEvent("a#clearResults", 'click');
-      //clearResults.trigger("click");
-      clearResults.click();
-                  
-      expect('click').toHaveBeenTriggeredOn("a#clearResults");
-      expect(spyEvent).toHaveBeenTriggered();
+          describe(
+                  "clicking the clearResults anchor with values in search box text inputs",
+                  function() {
+                    it("should clear search box on clearResults click",
+                            function() {
+                              spyEvent = spyOnEvent("a#clearResults", 'click');
+                              //clearResults.trigger("click");
+                              clearResults.click();
 
+                              expect('click').toHaveBeenTriggeredOn(
+                                      "a#clearResults");
+                              expect(spyEvent).toHaveBeenTriggered();
 
-        expect(patientQuery).toHaveValue("XXX");
-        //expect(patientQuery).not.toHaveValue("Hornblower");
-    });
-  });
-});
+                              expect(patientQuery).toHaveValue("XXX");
+                              //expect(patientQuery).not.toHaveValue("Hornblower");
+                            });
+                  });
+        });
